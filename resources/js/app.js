126d89eaeec4f9c8b41c0e2fb9bde63628f36492
Samuele Paratore <byhyons@gmail.com>
2024-09-05 12:20:18 +0200

    Initial commit

---
+++ resources/js/app.js
@@ -0,0 +1,14 @@
+import './bootstrap';
+
+//? import scss:
+import '~resources/scss/app.scss';
+
+//? import boostrap:
+import * as bootstrap from 'bootstrap';
+
+//? importo il componente modale:
+import './components/modale';
+
+//? import per le immagini:
+import.meta.glob(['../img/**']);
+
