8fa833aafc504499aeb2d80211f44c47f25b46a4
Rachele-Turtora <racheleturtora21@gmail.com>
2024-09-09 13:48:44 +0200

    [UPD] apartments list (config)

---
+++ config/apartments.php
@@ -0,0 +1,184 @@
+<?php
+
+return [
+    [
+        "title" => "Appartamento al Centro Storico",
+        "description" => "Accogliente appartamento nel cuore della città, vicino a tutti i servizi.",
+        "beds" => 2,
+        "bathrooms" => 1,
+        "rooms" => 3,
+        "mq" => 80,
+        "address" => "Via Roma 1, Roma"
+    ],
+    [
+        "title" => "Villetta con Giardino",
+        "description" => "Spaziosa villetta immersa nel verde con giardino privato.",
+        "beds" => 4,
+        "bathrooms" => 2,
+        "rooms" => 5,
+        "mq" => 150,
+        "address" => "Via dei Giardini 10, Milano"
+    ],
+    [
+        "title" => "Attico Vista Mare",
+        "description" => "Luminoso attico con splendida vista sul mare.",
+        "beds" => 3,
+        "bathrooms" => 2,
+        "rooms" => 4,
+        "mq" => 120,
+        "address" => "Via del Mare 5, Napoli"
+    ],
+    [
+        "title" => "Appartamento Moderno",
+        "description" => "Appartamento completamente ristrutturato in stile moderno.",
+        "beds" => 2,
+        "bathrooms" => 1,
+        "rooms" => 3,
+        "mq" => 90,
+        "address" => "Via Garibaldi 15, Torino"
+    ],
+    [
+        "title" => "Casa Vacanze sul Lago",
+        "description" => "Casa vacanze con accesso diretto al lago e vista panoramica.",
+        "beds" => 2,
+        "bathrooms" => 1,
+        "rooms" => 3,
+        "mq" => 100,
+        "address" => "Via Lungolago 20, Como"
+    ],
+    [
+        "title" => "Appartamento in Zona Universitaria",
+        "description" => "Perfetto per studenti, a pochi passi dall'università.",
+        "beds" => 1,
+        "bathrooms" => 1,
+        "rooms" => 2,
+        "mq" => 60,
+        "address" => "Via Zamboni 33, Bologna"
+    ],
+    [
+        "title" => "Appartamento in Villa d'Epoca",
+        "description" => "Splendido appartamento in villa d'epoca, immerso nel verde.",
+        "beds" => 3,
+        "bathrooms" => 2,
+        "rooms" => 4,
+        "mq" => 130,
+        "address" => "Via Verdi 5, Firenze"
+    ],
+    [
+        "title" => "Monolocale Arredato",
+        "description" => "Monolocale completamente arredato in zona centrale.",
+        "beds" => 1,
+        "bathrooms" => 1,
+        "rooms" => 1,
+        "mq" => 40,
+        "address" => "Corso Umberto I 10, Bari"
+    ],
+    [
+        "title" => "Trilocale con Vista",
+        "description" => "Appartamento trilocale con vista mozzafiato sulla città.",
+        "beds" => 2,
+        "bathrooms" => 1,
+        "rooms" => 3,
+        "mq" => 85,
+        "address" => "Via Toledo 12, Napoli"
+    ],
+    [
+        "title" => "Attico con Terrazza",
+        "description" => "Attico con ampia terrazza in zona residenziale tranquilla.",
+        "beds" => 3,
+        "bathrooms" => 2,
+        "rooms" => 4,
+        "mq" => 140,
+        "address" => "Via Dante Alighieri 8, Verona"
+    ],
+    [
+        "title" => "Loft Industriale",
+        "description" => "Loft dal design industriale in ex fabbrica ristrutturata.",
+        "beds" => 2,
+        "bathrooms" => 1,
+        "rooms" => 2,
+        "mq" => 100,
+        "address" => "Via Bovisasca 16, Milano"
+    ],
+    [
+        "title" => "Quadrilocale Signorile",
+        "description" => "Ampio quadrilocale signorile in palazzo storico.",
+        "beds" => 4,
+        "bathrooms" => 2,
+        "rooms" => 5,
+        "mq" => 160,
+        "address" => "Piazza della Signoria 1, Firenze"
+    ],
+    [
+        "title" => "Bilocale in Zona Tranquilla",
+        "description" => "Bilocale perfetto per coppie, situato in una zona tranquilla.",
+        "beds" => 1,
+        "bathrooms" => 1,
+        "rooms" => 2,
+        "mq" => 65,
+        "address" => "Via San Martino 22, Pisa"
+    ],
+    [
+        "title" => "Casa Indipendente in Campagna",
+        "description" => "Casa indipendente con ampio giardino nella tranquilla campagna toscana.",
+        "beds" => 3,
+        "bathrooms" => 2,
+        "rooms" => 4,
+        "mq" => 180,
+        "address" => "Strada di Montelupo 30, Siena"
+    ],
+    [
+        "title" => "Appartamento Vicino al Mare",
+        "description" => "Appartamento a pochi passi dal mare, ideale per le vacanze.",
+        "beds" => 2,
+        "bathrooms" => 1,
+        "rooms" => 3,
+        "mq" => 75,
+        "address" => "Via delle Sirene 12, Cagliari"
+    ],
+    [
+        "title" => "Appartamento Storico",
+        "description" => "Appartamento in edificio storico, con soffitti alti e affreschi.",
+        "beds" => 3,
+        "bathrooms" => 2,
+        "rooms" => 4,
+        "mq" => 140,
+        "address" => "Via dei Calzaiuoli 5, Firenze"
+    ],
+    [
+        "title" => "Trilocale Ristrutturato",
+        "description" => "Trilocale completamente ristrutturato con finiture di pregio.",
+        "beds" => 2,
+        "bathrooms" => 1,
+        "rooms" => 3,
+        "mq" => 90,
+        "address" => "Via Manzoni 22, Milano"
+    ],
+    [
+        "title" => "Appartamento con Giardino Privato",
+        "description" => "Appartamento al piano terra con giardino privato.",
+        "beds" => 2,
+        "bathrooms" => 1,
+        "rooms" => 3,
+        "mq" => 95,
+        "address" => "Via Garibaldi 30, Genova"
+    ],
+    [
+        "title" => "Monolocale in Centro",
+        "description" => "Monolocale in pieno centro, perfetto per single o giovani coppie.",
+        "beds" => 1,
+        "bathrooms" => 1,
+        "rooms" => 1,
+        "mq" => 45,
+        "address" => "Via Rizzoli 8, Bologna"
+    ],
+    [
+        "title" => "Villa con Piscina",
+        "description" => "Lussuosa villa con piscina e giardino privato, a pochi km dalla città.",
+        "beds" => 5,
+        "bathrooms" => 3,
+        "rooms" => 6,
+        "mq" => 250,
+        "address" => "Via del Colle 15, Firenze"
+    ]
+];
