655b52a1cd47dd34ef116a5a37c4ac6c1e3ef10d
Rachele-Turtora <racheleturtora21@gmail.com>
2024-09-09 16:38:15 +0200

    [UPD] home seeder

--- database/seeders/HomeSeeder.php
+++ database/seeders/HomeSeeder.php
@@ -2,8 +2,11 @@
 
 namespace Database\Seeders;
 
+use App\Models\Home;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Str;
 
 class HomeSeeder extends Seeder
 {
@@ -12,6 +15,25 @@ class HomeSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        Schema::disableForeignKeyConstraints();
+
+        $apartments = config('apartments');
+
+        foreach ($apartments as $new_apartment) {
+            $apartment = new Home();
+
+            $apartment->title = $new_apartment['title'];
+            $apartment->slug = Str::of($new_apartment->title)->slug('-');
+            $apartment->description = $new_apartment['description'];
+            $apartment->beds = $new_apartment['beds'];
+            $apartment->bathrooms = $new_apartment['bathrooms'];
+            $apartment->rooms = $new_apartment['rooms'];
+            $apartment->square_metres = $new_apartment['mq'];
+            $apartment->address = $new_apartment['address'];
+
+            $apartment->save();
+        }
+
+        Schema::enableForeignKeyConstraints();
     }
 }
