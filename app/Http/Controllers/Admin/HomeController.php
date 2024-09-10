d2881bc2b9c2f823b0197ef2426b896ef9d6dc4e
gianluigi7 <gianluigiferraioli7@gmail.com>
2024-09-10 16:05:07 +0200

    [UPD] home controller

--- app/Http/Controllers/Admin/HomeController.php
+++ app/Http/Controllers/Admin/HomeController.php
@@ -6,6 +6,8 @@ use App\Http\Controllers\Controller;
 use App\Http\Requests\StoreHomeRequest;
 use App\Http\Requests\UpdateHomeRequest;
 use App\Models\Home;
+use App\Models\Service;
+use Illuminate\Support\Str;
 
 class HomeController extends Controller
 {
@@ -24,7 +26,8 @@ class HomeController extends Controller
      */
     public function create()
     {
-        //
+        $services = Service::all();
+        return view('admin.homes.create', compact('services')); //compact('services')
     }
 
     /**
@@ -32,15 +35,34 @@ class HomeController extends Controller
      */
     public function store(StoreHomeRequest $request)
     {
-        //
+        $data = $request->validated();
+        $data['slug'] = Str::of($data['title'])->slug();
+    
+        $apartment = new Home();
+        $apartment->title = $data['title'];
+        $apartment->description = $data['description'];
+        $apartment->beds = $data['beds'];
+        $apartment->bathrooms = $data['bathrooms'];
+        $apartment->rooms = $data['rooms'];
+        $apartment->square_metres = $data['square_metres'];
+        $apartment->address = $data['address'];
+        $apartment->image = $data['image'];
+
+        $apartment->save();
+        if ($request->has('service')) {
+            $apartment->service()->attach($request->service);
+        }
+        return redirect()->route('admin.homes.index')->with('message', 'creazione avvenuta con successo');
     }
 
     /**
      * Display the specified resource.
      */
-    public function show(Home $home)
+    public function show(string $id)
     {
-        //
+        $apartment = Home::find($id);
+        $services = Service::all();
+        return view('admin.homes.show', compact('apartment', 'services'));
     }
 
     /**
@@ -48,22 +70,48 @@ class HomeController extends Controller
      */
     public function edit(Home $home)
     {
-        //
+        $services = Service::all();
+        return view('admin.homes.edit', compact('services'));
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(UpdateHomeRequest $request, Home $home)
+    public function update(UpdateHomeRequest $request, Home $apartment)
     {
-        //
+        $data = $request->validated();
+        $data['slug'] = Str::of($data['title'])->slug();
+
+
+        $apartment->title = $data['title'];
+        $apartment->description = $data['description'];
+        $apartment->beds = $data['beds'];
+        $apartment->bathrooms = $data['bathrooms'];
+        $apartment->rooms = $data['rooms'];
+        $apartment->square_metres = $data['square_metres'];
+        $apartment->address = $data['address'];
+        $apartment->image = $data['image'];
+        
+        $apartment->update($data);
+
+        $apartment->save();
+
+        if($request->has('services')) {
+
+            $apartment->services()->sync($request->services);
+        }
+        else {
+            $apartment->services()->detach();
+        }
+        return redirect()->route('admin.projects.index');
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(Home $home)
+    public function destroy(Home $apartment)
     {
-        //
+        $apartment->delete();
+        return redirect()->route('admin.home.index');
     }
 }
