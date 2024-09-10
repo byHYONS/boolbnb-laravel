5241599d3a66ac5b909f8f35424ea4be3ef8a7a2
byHYONS <byhyons@gmail.com>
2024-09-10 11:53:03 +0200

    feat: aggiunto seeder tabelle pivot

--- database/seeders/HomeService.php
+++ database/seeders/HomeService.php
@@ -2,41 +2,38 @@
 
 namespace Database\Seeders;
 
-use App\Models\Ad;
-use App\Models\AdHome;
 use App\Models\Home;
+use App\Models\HomeService as ModelsHomeService;
+use App\Models\Service;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\Schema;
 
-class AdHomeSeeder extends Seeder
+class HomeService extends Seeder
 {
     /**
      * Run the database seeds.
      */
     public function run(): void
     {
-        
         //? disabilito relazioni:
         Schema::disableForeignKeyConstraints();
 
         //? ripulisco tabella:
-        AdHome::truncate();
+        ModelsHomeService::truncate();
 
         //? popoliamo random la tabella pivot:
-        for ($i = 0; $i < 10; $i++) {
-            $ad_home = new AdHome();
+        for ($i = 0; $i < 20; $i++) {
+            $home_service = new ModelsHomeService();
 
-            $ad_home->ad_id = Ad::inRandomOrder()->first()->id;
-            $ad_home->home_id = Home::inRandomOrder()->first()->id;
+            $home_service->home_id = Home::inRandomOrder()->first()->id;
+            $home_service->service_id = Service::inRandomOrder()->first()->id;
 
-            $ad_home->save();
+            $home_service->save();
 
         }
 
         //? abilito relazione:
         Schema::enableForeignKeyConstraints();
-
-
     }
 }
