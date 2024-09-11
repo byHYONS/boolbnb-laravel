ccfc5b352ac2745362879827f0471e7ee64a8e0e
byHYONS <byhyons@gmail.com>
2024-09-11 16:23:02 +0200

    fix: corretto regola request

--- app/Http/Requests/UpdateHomeRequest.php
+++ app/Http/Requests/UpdateHomeRequest.php
@@ -3,6 +3,7 @@
 namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
 
 class UpdateHomeRequest extends FormRequest
 {
@@ -22,7 +23,13 @@ class UpdateHomeRequest extends FormRequest
     public function rules(): array
     {
         return [
-            'title' => 'required|min:5|max:50|string',
+            'title' => [
+                'required',
+                'string',
+                'min:5',
+                'max:50',
+                Rule::unique('homes', 'title')->ignore($this->home),
+            ],
             'description' => 'nullable|min:10|max:250|string',
             'beds' => 'required|integer',
             'bathrooms' => 'required|integer',
