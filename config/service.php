ff1c7d6d491d727f8c65305101edf0d1559c5e52
Rachele-Turtora <racheleturtora21@gmail.com>
2024-09-17 11:01:33 +0200

    [UPD] add services

--- config/service.php
+++ config/service.php
@@ -4,7 +4,7 @@ return [
     [
         "id" => 1,
         "name" => "WiFi",
-        "icon" => "fa-solid fa-wifi", 
+        "icon" => "fa-solid fa-wifi",
     ],
     [
         "id" => 2,
@@ -30,5 +30,25 @@ return [
         "id" => 6,
         "name" => "Vista Mare",
         "icon" => "fa-solid fa-water",
-    ]
+    ],
+    [
+        "id" => 7,
+        "name" => "TV",
+        "icon" => "fa solid fa-tv"
+    ],
+    [
+        "id" => 8,
+        "name" => "Ascensore",
+        "icon" => "fa-solid fa-elevator"
+    ],
+    [
+        "id" => 9,
+        "name" => "Aria condizionata",
+        "icon" => "fa-solid fa-fan"
+    ],
+    [
+        "id" => 10,
+        "name" => "Kit medico",
+        "icon" => "fa-solid fa-kit-medical"
+    ],
 ];
