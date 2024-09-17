ccc6510c6dd6c3349c9c6a56a086092b42104da6
Rachele-Turtora <racheleturtora21@gmail.com>
2024-09-17 12:02:04 +0200

    [UPD] services + seeder

--- config/service.php
+++ config/service.php
@@ -4,51 +4,51 @@ return [
     [
         "id" => 1,
         "name" => "WiFi",
-        "icon" => "fa-solid fa-wifi",
+        "icon" => "wifi",
     ],
     [
         "id" => 2,
         "name" => "Posto Macchina",
-        "icon" => "fa-solid fa-car-side",
+        "icon" => "car-side",
     ],
     [
         "id" => 3,
         "name" => "Piscina",
-        "icon" => "fa-solid fa-person-swimming",
+        "icon" => "person-swimming",
     ],
     [
         "id" => 4,
         "name" => "Portineria",
-        "icon" => "fa-solid fa-person-shelter",
+        "icon" => "person-shelter",
     ],
     [
         "id" => 5,
         "name" => "Sauna",
-        "icon" => "fa-solid fa-temperature-arrow-up",
+        "icon" => "temperature-arrow-up",
     ],
     [
         "id" => 6,
         "name" => "Vista Mare",
-        "icon" => "fa-solid fa-water",
+        "icon" => "water",
     ],
     [
         "id" => 7,
         "name" => "TV",
-        "icon" => "fa solid fa-tv"
+        "icon" => "tv"
     ],
     [
         "id" => 8,
         "name" => "Ascensore",
-        "icon" => "fa-solid fa-elevator"
+        "icon" => "elevator"
     ],
     [
         "id" => 9,
         "name" => "Aria condizionata",
-        "icon" => "fa-solid fa-fan"
+        "icon" => "fan"
     ],
     [
         "id" => 10,
         "name" => "Kit medico",
-        "icon" => "fa-solid fa-kit-medical"
+        "icon" => "kit-medical"
     ],
 ];
