f532d9914a24ec89cd71984b8c11fe46088e670b
byHYONS <byhyons@gmail.com>
2024-09-25 20:36:16 +0200

    fix: homepage

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,31 +1,8 @@
 @extends('layouts.app')
 @section('content')
-    {{-- <div class="welcome">
-        <div class="jumbotron p-5 mb-4 bg-light rounded-3">
-            <div class="container py-5">
-    
-                <h1 class="display-5 fw-bold">
-                    Welcome to Laravel+Bootstrap
-                </h1>
-    
-                <p class="col-md-8 fs-4">Using a series of utilities, you can create this jumbotron, just like the one in
-                    previous versions of Bootstrap. Check out the examples below for how you can remix and restyle it to your
-                    liking.</p>
-                <button class="button" type="button">Example button</button>
-            </div>
-        </div>
-    
-        <div class="content">
-            <div class="container">
-                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Tempora temporibus, dicta nemo aliquam totam nisi
-                    deserunt soluta quas voluptatum ab beatae praesentium necessitatibus minus, facilis illum rerum officiis
-                    accusamus dolores!</p>
-            </div>
-        </div>
-    </div> --}}
-
     <section id="welcome">
         <div class="home-show">
+            {{--? headline --}}
             <div class="bg01">
                 <div class="container py-75">
                     <div class="headline ">
@@ -37,6 +14,7 @@
                     </div>
                 </div>
             </div>
+            {{--? subheadline --}}
             <div class="subheadline">
                 <div class="container mt-75">
                     <p>
@@ -48,6 +26,7 @@
                     <h3 class="mt-4"> 
                         Non aspettare <span>scegli BoolBnb</span> per dare al tuo immobile l’<span>attenzione che merita</span>
                     </h3>
+                    {{--? bottone --}}
                     <div class="manage">
                         <div class="create py-5">
                             <a href="{{route('admin.homes.create') }}">{{ __('Crea Nuovo')}}</a>
@@ -56,6 +35,5 @@
                 </div>
             </div>
         </div>
-
     </section>
 @endsection
